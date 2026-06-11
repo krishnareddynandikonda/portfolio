4778762f80746ee2e5bd1797ae5d1d9ebf16d7d1
krishnareddynandikonda <srividya.deshaboinasri@gmail.com>
2026-06-11 21:32:46 +0200

    Set Pages base path to /portfolio/

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,9 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// Hosted as a GitHub Pages project site at username.github.io/krishnareddy/
-// so assets must be served from the /krishnareddy/ base path.
+// Hosted as a GitHub Pages project site at krishnareddynandikonda.github.io/portfolio/
+// so assets must be served from the /portfolio/ base path.
 export default defineConfig({
-  base: '/krishnareddy/',
+  base: '/portfolio/',
   plugins: [react()],
 })
